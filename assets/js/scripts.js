fa61d199189caf3aa90a7edff73dce614be22938
Terrance <Terrancedblakely@gmail.com>
2022-05-29 08:56:38 -0400

    Update styles.css, scripts.js and one other file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,22 @@
-let navMenuBar = document.getElementById('navMenuBar')
 let openNavBtn = document.getElementById('openNavBtn')
+let navMenu = document.getElementById('navMenuBar')
+let closeNavBtn = document.getElementById('closeNavBtn')
 
 openNavBtn.addEventListener('click', openNavMenu)
 
 function openNavMenu() {
-    navMenuBar.style.marginLeft = '-15rem'
+	event.preventDefault()
+	navMenu.style.marginLeft = '0'
+	console.log("Opening Navigation")	
+	// return false;
 }
 
+closeNavBtn.addEventListener('click', closeNavMenu)
 
+function closeNavMenu() {
+	event.preventDefault()
+	navMenu.style.marginLeft = '-15rem'
+	console.log("Closing Navigation")
+}
 
 
