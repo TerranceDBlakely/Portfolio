597d2b53a091fee8525909d7c2c9953070b6c95b
Terrance <Terrancedblakely@gmail.com>
2022-05-31 19:06:19 -0400

    Update about.html, styles.css and four other files

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,32 @@
 let navMenuBar = document.getElementById('navMenuBar')
-let openNavBtn = document.getElementById('openNavBtn')
-let closeNavBtn = document.getElementById('closeNavBtn')
+let openNavBtn = document.getElementById('openNavBtn').addEventListener('click', openNavMenu)
+let closeNavBtn = document.getElementById('closeNavBtn').addEventListener('click', closeNavMenu)
+let pageContainer = document.getElementById('pageContainer')
 
-let openBtn = openNavBtn.addEventListener('click', openNavMenu)
-let closeBtn = closeNavBtn.addEventListener('click', closeNavMenu)
+// 1
+// - 
+
+// 2
+// - If the navbar open
+// - Then make the openNavBtn not display itself
+navMenuBar.style.marginLeft = '-15rem'
 
 function openNavMenu() {
 		event.preventDefault()
 		navMenuBar.style.marginLeft = '0'
+		// pageContainer.style.marginLeft = '15rem'
 		console.log("Opening Sidebar")	
 }
 	
 function closeNavMenu() {
 	event.preventDefault()
-	navMenuBar.style.marginLeft = '-30rem'
+	navMenuBar.style.marginLeft = '-15rem'
+	// pageContainer.style.marginLeft = '0'
 	console.log("Closing Sidebar")
 }
+
+// if (navMenuBar.style.width = '30rem') {
+// 	console.log('Your Menu is not showing right now')
+// } else
+
 
