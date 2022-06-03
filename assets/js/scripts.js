dae0e36308d3ef55faa1c00d6bf954ac102bc588
Terrance <Terrancedblakely@gmail.com>
2022-06-03 18:58:17 -0400

    Update styles.css and scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,26 +4,12 @@ let navMenuBar = document.getElementById('navMenuBar')
 let openNavBtn = document.getElementById('openNavBtn')
 let closeNavBtn = document.getElementById('closeNavBtn')
 
-var menu = {
-	open: false,
-	marginLeft: '-20rem',	
-	getRect: function() {
-		return navMenuBar.getBoundingClient()
-	}
-}
 
-function toggleMenu() {
-	// pageContainer 
-	
-	if (menu.open) {
-		navMenuBar.style.marginLeft = '-20rem'
-		menu.open = false
-	}
-	else {		
-		navMenuBar.style.marginLeft = '0'
-		menu.open = true
-		}		
-}
+let closed = navMenuBar.style.display = 'none'
+
+openNavBtn.addEventListener('click', openMenu(){
+	let open = navMenuBar.style.diplay = 'block'
+}, false)
 
 
 
@@ -66,21 +52,10 @@ function toggleMenu() {
 
 
 
-// function openNavigationBar() {
-// 	navMenuBar.style.marginLeft = '0'
-// 	// event.preventDefault()
-// 	pageContainer.style.marginLeft = '15rem'
-// 	console.log('Navigation is Opening')
-// }
 
-// closeNavBtn.addEventListener('click', closeNavigationBar, false)
 
-// function closeNavigationBar() {
-// 	navMenuBar.style.marginLeft = '-25rem'
-// 	// event.preventDefault()
-// 	pageContainer.style.marginLeft = '0'
-// 	console.log('Navigation is Closing')
-// }
+
+
 
 
 
