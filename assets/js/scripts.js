07056b8349a8529c9fb8361979d6b5ada6e34b9a
Terrance <Terrancedblakely@gmail.com>
2022-06-03 04:03:46 -0400

    Update styles.css, scripts.js and one other file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,86 @@
 // Navigation Script 
+
 let navMenuBar = document.getElementById('navMenuBar')
 let openNavBtn = document.getElementById('openNavBtn')
 let closeNavBtn = document.getElementById('closeNavBtn')
-let pageContainer = document.getElementById('pageContainer')
 
-
-navMenuBar.style.marginLeft = '-25rem'
-
-
-openNavBtn.addEventListener('click', openNavigationBar, false)
-
-function openNavigationBar() {
-	navMenuBar.style.marginLeft = '0'
-	event.preventDefault()
-	pageContainer.style.marginLeft = '15rem'
+var menu = {
+	open: false,
+	marginLeft: '-20rem',	
+	getRect: function() {
+		return navMenuBar.getBoundingClient()
+	}
 }
 
-closeNavBtn.addEventListener('click', closeNavigationBar, false)
-
-function closeNavigationBar() {
-	navMenuBar.style.marginLeft = '-25rem'
-	event.preventDefault()
-	pageContainer.style.marginLeft = '0'
+function toggleMenu() {
+	// pageContainer 
+	
+	if (menu.open) {
+		navMenuBar.style.marginLeft = '-20rem'
+		menu.open = false
+	}
+	else {		
+		navMenuBar.style.marginLeft = '0'
+		menu.open = true
+		}		
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function openNavigationBar() {
+// 	navMenuBar.style.marginLeft = '0'
+// 	// event.preventDefault()
+// 	pageContainer.style.marginLeft = '15rem'
+// 	console.log('Navigation is Opening')
+// }
+
+// closeNavBtn.addEventListener('click', closeNavigationBar, false)
+
+// function closeNavigationBar() {
+// 	navMenuBar.style.marginLeft = '-25rem'
+// 	// event.preventDefault()
+// 	pageContainer.style.marginLeft = '0'
+// 	console.log('Navigation is Closing')
+// }
 
 
 
