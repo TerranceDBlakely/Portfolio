f793db789e62bce0103180d763b5b1a43ec7baa2
Terrance <Terrancedblakely@gmail.com>
2022-06-05 16:48:57 -0400

    Update styles.css and scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,19 @@
 // Navigation Script 
 
-let navMenuBar = document.getElementById('navMenuBar')
-let openNavBtn = document.getElementById('openNavBtn')
+// function toggleNavigationMenu() {
+// 	if(navMenuBar.className = 'navMenu'){
+// 			navMenuBar.className = 'navMenuClosed'
+// 			console.log('nav menu has changed to "NavMenuClosed" ')
+// 		}
+// }
 
-function toggleNavigationMenu() {
-	if(navMenuBar.className = 'navMenu'){
-			navMenuBar.className = 'navMenuClosed'
-			console.log('nav menu has changed to "NavMenuClosed" ')
-		}
-}
+// openNavBtn.addEventListener('click', toggleNavigationMenu, false)
 
-openNavBtn.addEventListener('click', toggleNavigationMenu, false)
+if(document.getElementById('navMenuBar').style.marginLeft = '-20rem')
+{
+	document.getElementById('openNavBtn').addEventListener('click', function() 
+		{
+			document.getElementById('navMenuBar').style.marginLeft = '0rem'		
+	})
+}	
 
