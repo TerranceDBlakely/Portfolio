8f5daec9257b23e2a558f30d1f72ed76dbbd0264
Terrance <Terrancedblakely@gmail.com>
2022-08-14 18:02:46 -0400

    New version

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,3 @@
-// Navigation Script 
 
-// function toggleNavigationMenu() {
-// 	if(navMenuBar.className = 'navMenu'){
-// 			navMenuBar.className = 'navMenuClosed'
-// 			console.log('nav menu has changed to "NavMenuClosed" ')
-// 		}
-// }
 
-// openNavBtn.addEventListener('click', toggleNavigationMenu, false)
-
-if(document.getElementById('navMenuBar').style.marginLeft = '-20rem')
-{
-	document.getElementById('openNavBtn').addEventListener('click', function() 
-		{
-			document.getElementById('navMenuBar').style.marginLeft = '0rem'		
-	})
-}	
 
